19c335c11f94dd9d8968919482c098b7873c76b2
Marcelo Stabile <marcelo.stabile@correo.ucu.edu.uy>
2021-09-14 00:16:27 -0300

    .

--- AED2-ABC/src/main.java
+++ AED2-ABC/src/main.java
@@ -13,6 +13,8 @@ public class main {
          * Listas de entrada.
          */
         int[] miListaDesordenada = {5,6,10,18,8,9,9,10,3,9};
+        //int[] miListaDesordenada = {6,1,9,4,8,2,5,7,3};
+        //int[] miListaDesordenada = {5,6,10,18,8,9,9,10,3,9};
         //int[] miListaDesordenada = {89, 3, 79, 43, 5, 67, 13, 61, 17, 31, 37, 47, 29, 53, 23, 2, 59, 71, 11, 73, 19, 83, 7, 41, 97};
         int[] miListaOrdenada = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
